130a4de15284022461d516fcf89a6c0ddb0f7873
trgomes <trtg10@gmail.com>
2016-03-09 10:21:17 -0300

    Exercicio pilha finalizado

--- Exercicios/3-pilha.py
+++ Exercicios/3-pilha.py
@@ -6,25 +6,25 @@ class PilhaVaziaErro(Exception):
 
 class Pilha():
     def __init__(self):
-        self.topo = None
         self.lista = []
 
+    def topo(self):
+        if self.lista:
+            return self.lista[-1]
+        raise PilhaVaziaErro()
+
     def vazia(self):
-        if len(self.lista) == 0:
-            return True
-        else:
-            return False
+        return not bool(self.lista)
 
     def empilhar(self, valor):
         self.lista.append(valor)
-        self.topo = self.lista[-1]
 
 
     def desempilhar(self):
-        if len(self.lista) == 0:
-            return None
-        else:
+        try:
             return self.lista.pop()
+        except IndexError:
+            raise PilhaVaziaErro
 
 
 import unittest
@@ -40,18 +40,18 @@ class PilhaTestes(unittest.TestCase):
         pilha = Pilha()
         pilha.empilhar('A')
         self.assertFalse(pilha.vazia())
-        self.assertEqual('A', pilha.topo)
+        self.assertEqual('A', pilha.topo())
 
     def test_empilhar_dois_elementos(self):
         pilha = Pilha()
         pilha.empilhar('A')
         pilha.empilhar('B')
         self.assertFalse(pilha.vazia())
-        self.assertEqual('B', pilha.topo)
+        self.assertEqual('B', pilha.topo())
 
     def test_desempilhar_pilha_vazia(self):
         pilha = Pilha()
-        self.assertRaises(PilhaVaziaErro, pilha.desempilhar())
+        self.assertRaises(PilhaVaziaErro, pilha.desempilhar)
 
     def test_desempilhar(self):
         pilha = Pilha()
