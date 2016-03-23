51548b219760ebf9131031cd54e14238eb0015f0
thiago <trtg10@gmail.com>
2016-03-23 12:11:40 -0300

    Exercico bubble_sort ok

---
+++ aula6/bubble_sort.py
@@ -0,0 +1,33 @@
+
+import unittest
+
+
+def bubble_sort(seq):
+
+    for i in range(len(seq)-1):
+
+        for x in range(len(seq)-1):
+            if seq[x] > seq[x+1]:
+               seq[x], seq[x+1] = seq[x+1], seq[x]
+               if i == 0: cont = 1
+        if cont and cont != 1: break
+
+    return seq
+
+
+class OrdenacaoTestes(unittest.TestCase):
+    def teste_lista_vazia(self):
+        self.assertListEqual([], bubble_sort([]))
+
+    def teste_lista_unitaria(self):
+        self.assertListEqual([1], bubble_sort([1]))
+
+    def teste_lista_binaria(self):
+        self.assertListEqual([1, 2], bubble_sort([2, 1]))
+
+    def teste_lista_binaria(self):
+        self.assertListEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], bubble_sort([9, 7, 1, 8, 5, 3, 6, 4, 2, 0]))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ aula6/selection_sort.py
@@ -0,0 +1,28 @@
+import unittest
+
+
+def selection_sort(seq):
+    n = len(seq)
+    for i_corrente in range(n):
+        _, i_min = min((seq[i], i) for i in range(i_corrente, n))
+        seq[i_corrente], seq[i_min] = seq[i_min], seq[i_corrente]  # swap
+    return seq
+
+
+class OrdenacaoTestes(unittest.TestCase):
+    def teste_lista_vazia(self):
+        self.assertListEqual([], selection_sort([]))
+
+    def teste_lista_unitaria(self):
+        self.assertListEqual([1], selection_sort([1]))
+
+    def teste_lista_binaria(self):
+        self.assertListEqual([1, 2], selection_sort([2, 1]))
+
+    def teste_lista_binaria(self):
+        self.assertListEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], selection_sort([9, 7, 1, 8, 5, 3, 6, 4, 2, 0]))
+
+
+if __name__ == '__main__':
+    unittest.main()
+
